cc97104f424b3a3dfb9bb5fc55e324bf4d33881a
Anna <kayzerscake@gmail.com>
2024-01-11 18:37:47 -0500

    js

--- script.js
+++ script.js
@@ -26,13 +26,14 @@ const searchForm = document.querySelector('#location-form');
 searchForm.addEventListener("submit", function (event) {
   event.preventDefault()
   getAPI()
+  getWeek()
 });
 
 console.log (searchInput.value);
 
 // Make API request
 function getAPI() {
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`)
   .then(response => response.json())
   .then(data => {
     // weather data from API
@@ -51,3 +52,37 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&ap
     console.error('Error:', error);
   });
 };
+
+function getWeek() {
+    // forecast info
+    const forecastData = data.list;
+
+    // html
+    forecastData.forEach(day => {
+      const date = day.dt_txt;
+      const temperature = day.main.temp;
+      const weatherIcon = day.weather[0].icon;
+
+      const forecastItem = document.createElement('div');
+      forecastItem.classList.add('week');
+
+      const dateElement = document.createElement('p');
+      dateElement.textContent = date;
+      forecastItem.appendChild(dateElement);
+
+      const temperatureElement = document.createElement('p');
+      temperatureElement.textContent = temperature;
+      forecastItem.appendChild(temperatureElement);
+
+      const weatherIconElement = document.createElement('img');
+      weatherIconElement.src = `http://openweathermap.org/img/w/${weatherIcon}.png`;
+      forecastItem.appendChild(weatherIconElement);
+
+      // append in html
+      const forecastContainer = document.getElementById('forecast-container');
+      forecastContainer.appendChild(forecastItem);
+    });
+  });
+  .catch(error => {
+    console.log('Error:', error);
+  });
